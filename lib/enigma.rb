de72007c2e6b9cde8b68780f55c1b59383455908
Wil McCauley <wmccauley724@gmail.com>
2021-01-14 15:53:06 -0700

    Add key gen method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -6,13 +6,15 @@ class Enigma
 
   def initialize
     @chars = ("a".."z").to_a << " "
-    @nums  = (0..9).to_a
+    @nums  = ('0'..'9').to_a
   end
 
   def key_gen
+    key_str = ""
     5.times do
-      @nums.sample
+      key_str += @nums.sample
     end
+    key_str
   end
 
   # Refactor me I'm long and hideous
