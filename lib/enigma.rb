f3d5fd922cb8f3b5ed67c2baf55220a24740e109
Wil McCauley <wmccauley724@gmail.com>
2021-01-14 15:11:48 -0700

    Add a time cleaner for reference

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,5 @@
+require 'Time'
+
 class Enigma
   attr_reader :chars,
               :nums
@@ -5,6 +7,17 @@ class Enigma
   def initialize
     @chars = ("a".."z").to_a << " "
     @nums  = (0..9).to_a
+  end
+
+  def key_gen
+    5.times do
+      @nums.sample
+    end
+  end
+
+  # Refactor me I'm long and hideous
+  def get_offset_number
+    Time.now.strftime('%d%m%Y').slice(0..3)
   end
 
 end
