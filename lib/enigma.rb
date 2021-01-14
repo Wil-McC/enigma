6b532d33b62cf2fc3fa1b40016185da945806f6f
Wil McCauley <wmccauley724@gmail.com>
2021-01-14 16:43:33 -0700

    Refactor shift methods and date converter to shift calculator, remove from enigma

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,24 +2,9 @@ require 'Time'
 
 class Enigma
   attr_reader :chars,
-              # :nums
 
   def initialize
     @chars = ("a".."z").to_a << " "
-    # @nums  = ('0'..'9').to_a
-  end
-
-  def key_gen
-    key_str = ""
-    5.times do
-      key_str += @nums.sample
-    end
-    key_str
-  end
-
-  # Refactor me I'm long and hideous
-  def get_offset_number
-    Time.now.strftime('%d%m%Y').slice(0..3)
   end
 
 end
--- lib/shift_calculator.rb
+++ lib/shift_calculator.rb
@@ -1,9 +1,29 @@
 class ShiftCalculator
+
   attr_reader :nums
 
   def initialize
     @nums = ('0'..'9').to_a
+    @key_base = key_gen
   end
+
+  def get_offset_number
+    Time.now.strftime('%d%m%Y').slice(0..3)
+  end
+
+  def key_gen
+    key_str = ""
+    5.times do
+      key_str += @nums.sample
+    end
+    key_str
+  end
+
+
+  # a shift = key_base[0..1]
+  # b shift
+  # c shift
+  # d shift
 
 
 end
