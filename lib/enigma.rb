a53107ff97c254a42f8d76ca3d1201c38fd480da
Wil McCauley <wmccauley724@gmail.com>
2021-01-16 14:45:34 -0700

    Add DDD ccode for index_of and encryption method framework

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -5,10 +5,16 @@ class Enigma
     @chars = ("a".."z").to_a << " "
   end
 
-  def encrypt(string, key, date)
+  def encrypt(string, key = @sc.key_base, date )
     string.chars.map do |char|
+      if @chars.include?(char)
+        @chars.rotate(index_of(char))[shift]
+      end
+    end.join
+  end
 
-    end.join('') # ???? flatten?
+  def index_of(char)
+    @chars.index(char)
   end
 
   def scrub_shift(shift)
