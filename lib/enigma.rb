993ea83119467b7f1bc49d7e9fc3cb6bb95d6722
Wil McCauley <wmccauley724@gmail.com>
2021-01-16 17:34:21 -0700

    Add code foir mostly functioning initial encryption algo

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,35 +3,38 @@ require_relative './shiftable'
 class Enigma
   include Shiftable
 
-  attr_reader :chars
+  attr_reader :char_array
 
   def initialize
     @char_array       = ("a".."z").to_a << " "
     @num_array        = ('0'..'9').to_a
     @key_base         = key_gen
     @date_string      = today_date_string
-    @shifts_by_flavor = Hash.new
+    @shifts_by_flavor = nil                   # leave in methods?
   end
 
   def encrypt(string, key = @key_base, date_string = @date_string)
-    key  # when not given
+    @shifts_by_flavor = shift_builder(key, date_string)
+    shift_val = @shifts_by_flavor['a'] # temp
     string.chars.map do |char|
       if @char_array.include?(char)
-        @char_array.rotate(index_of(char))[shift]
+        @char_array.rotate(index_of(char))[scrub_shift(shift_val)]
+      else
+        char
       end
     end.join
 
-    # crypt_out = line 16 map enum
+    # crypt_out = line 19 map enum
     # h[:encryption] = crypt_out
     # h[:key]        = key ## or key_base
     # h[:date]       = date_string
   end
 
   def index_of(char)
-    @chars.index(char)
+    @char_array.index(char)
   end
 
-  def scrub_shift(shift)
-    shift % 27
+  def scrub_shift(shift_val)
+    shift_val % 27
   end
 end
