e33dceb3dc48832a663d858e7b49905da53731b9
Wil McCauley <wmccauley724@gmail.com>
2021-01-16 17:43:24 -0700

    Add initial code for outputting desired info hash from encrypt

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -15,8 +15,8 @@ class Enigma
 
   def encrypt(string, key = @key_base, date_string = @date_string)
     @shifts_by_flavor = shift_builder(key, date_string)
-    shift_val = @shifts_by_flavor['a'] # temp
-    string.chars.map do |char|
+    shift_val = @shifts_by_flavor.values.sample # temp
+    en_out = string.chars.map do |char|
       if @char_array.include?(char)
         @char_array.rotate(index_of(char))[scrub_shift(shift_val)]
       else
@@ -24,10 +24,11 @@ class Enigma
       end
     end.join
 
-    # crypt_out = line 19 map enum
-    # h[:encryption] = crypt_out
-    # h[:key]        = key ## or key_base
-    # h[:date]       = date_string
+    en_hash = Hash.new
+    en_hash[:encryption] = en_out
+    en_hash[:key]        = key
+    en_hash[:date]       = date_string
+    en_hash
   end
 
   def index_of(char)
