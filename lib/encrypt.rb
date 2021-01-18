7e95a949bf78cae7ad94fcd1584cdcc616611a06
Wil McCauley <wmccauley724@gmail.com>
2021-01-18 11:44:11 -0700

    Add file read method, store encrypt output for writing

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -2,8 +2,14 @@ require_relative './enigma'
 
 enigma = Enigma.new
 
-message = File.open(ARGV[0], 'r')
+# message = File.open(ARGV[0], 'r')
+# message_string = message.read
+
+# or
+
+message_string = File.read(ARGV[0])
+output_hash = enigma.encrypt(message_string)
 
 output_file = File.open(ARGV[1], 'w')
-output_file.write(crypted_text)
+output_file.write(output_hash[:encrypted])
 output_file.close
