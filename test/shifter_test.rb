fcad5a92e09b97dfc1319305a3dd4260d9b19717
Wil McCauley <wmccauley724@gmail.com>
2021-01-17 17:21:41 -0700

    Add tests for module methods, add typed offset test

--- test/shifter_test.rb
+++ test/shifter_test.rb
@@ -1,6 +1,8 @@
 require_relative 'test_helper'
+require './lib/generator'
 
 class ShifterTest < Minitest::Test
+  include Generator
 
   def setup
     @shifter = Shifter.new('02715','040895')
@@ -38,15 +40,30 @@ class ShifterTest < Minitest::Test
     assert_equal expected, @shifter.decrypt('keder ohulw')
   end
 
-  # Module tests
+  def test_typed_offset
+    @shifter.stubs(:offset_number).returns('4321')
+
+    assert_equal 4, @shifter.typed_offset('a', '040895')
+    assert_equal 3, @shifter.typed_offset('b', '040895')
+    assert_equal 2, @shifter.typed_offset('c', '040895')
+    assert_equal 1, @shifter.typed_offset('d', '040895')
+  end
+
+  # Module tests - require module and get rid of shifter
   def test_it_gets_valid_offset_string
-    assert_equal 4, @shifter.offset_number('040895').length
-    assert_equal String, @shifter.offset_number('040895').class
+    assert_equal 4, offset_number('040895').length
+    assert_equal String, offset_number('040895').class
   end
 
   def test_date_string
-    assert_equal String, @shifter.today_date_string.class
-    assert_equal 8, @shifter.today_date_string.length
-    # stub time to test
+    assert_equal String, today_date_string.class
+    assert_equal 8, today_date_string.length
   end
+
+  def test_it_gets_valid_offset_string
+    assert_equal 4, offset_number('040895').length
+    assert_equal String, offset_number('040895').class
+    assert_equal '1025', offset_number('040895')
+  end
+
 end
