f97367e9b77b0fffc30157b94dc75d84c2555362
Wil McCauley <wmccauley724@gmail.com>
2021-01-18 12:09:11 -0700

    Add code for functioning encrypt runner

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,15 +1,13 @@
 require_relative './enigma'
 
 enigma = Enigma.new
+require "pry"; binding.pry
 
-# message = File.open(ARGV[0], 'r')
-# message_string = message.read
-
-# or
-
-message_string = File.read(ARGV[0])
+message_string = File.read('./lib/' + ARGV[0])
 output_hash = enigma.encrypt(message_string)
 
-output_file = File.open(ARGV[1], 'w')
-output_file.write(output_hash[:encrypted])
-output_file.close
+# output_file = File.open(ARGV[1], 'w')
+File.write(ARGV[1], output_hash[:encryption])
+# output_file.close
+
+puts "Created #{ARGV[1]} with the key #{output_hash[:key]} and date #{output_hash[:date]}"
