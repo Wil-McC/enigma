c746640b22d9d3b43f84b69597b0a5037064c330
Wil McCauley <wmccauley724@gmail.com>
2021-01-17 17:41:16 -0700

    Add test for num array method

--- test/generator_test.rb
+++ test/generator_test.rb
@@ -4,14 +4,26 @@ require './lib/generator'
 class GeneratorTest < Minitest::Test
   include Generator
 
-  def test_date_string
+  def test_today_date_string
     assert_equal String, today_date_string.class
     assert_equal 8, today_date_string.length
   end
 
-  def test_it_gets_valid_offset_string
+  def test_it_gets_valid_offset_number
     assert_equal 4, offset_number('040895').length
     assert_equal String, offset_number('040895').class
     assert_equal '1025', offset_number('040895')
+  end
+
+  def test_key_gen
+    assert_equal String, key_gen.class
+    assert_equal 5, key_gen.length
+  end
+
+  def test_it_has_num_array
+    def test_it_has_nums
+      assert_equal ('0'..'9').to_a, num_array
+      assert_equal 10, num_array.length
+    end
   end
 end
